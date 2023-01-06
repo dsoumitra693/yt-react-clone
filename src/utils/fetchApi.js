14f59591ddab41251fa0b0d6c5be3a11dcd8e6e1
unknown <dsoumitra693@gmail.com>
2023-01-06 15:38:31 +0530

    adding the enviornment variable

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL = process.env.REACT_APP_BASE_URL;
   
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}${url}`);
+    const { data } = await axios.get(`${BASE_URL}/${url}`);
     return data;
     
 }
